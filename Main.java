384bb55f43a28baef7407b28cf7ae114aab271fc
mcleczi <mcleczi@gmail.com>
2016-11-08 12:33:42 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -36,7 +36,7 @@ import javafx.scene.shape.Polygon;
 public class Main extends Application {
 	
 	// Global Variables
-	private int numberOfCards = -1;		// number of cards to be dealt
+	private int numberOfCards = -1;		// Number of cards to be dealt
 	
 	ObservableList<Integer> selectedShapes = FXCollections.observableArrayList(); // list of user-selected shapes to play the game with
 	ObservableList<NamedColor> selectedColors = FXCollections.observableArrayList(); // list of user-selected colors to play the game with
@@ -48,24 +48,32 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 
+			// AnchorPane for all buttons, labels, and list on initial screen, root is the center of the borderpane page
 			AnchorPane root = new AnchorPane();
 			
+			
+			// BorderPane as the base of the scene
 			BorderPane page = new BorderPane();
 			
+			
+			// Top of page, intro AnchorPane to be displayed before game() is called
 			AnchorPane intro = new AnchorPane(); 
 			
+			
+			// Create new scene 
 			Scene scene = new Scene(page,700,500);
+	
 			
 			
-			Label introLabel = new Label(" The Shapes Are Right!");
+			// This label is displayed at the top of the screen and acts as an into before the user starts playing the game
+			Label introLabel = new Label("  The Shapes Are Right!");
 			introLabel.setStyle( "-fx-background-color: #000000; -fx-text-fill: #eeeeee;  -fx-font-size: 60");
 			introLabel.setPrefWidth(700);
 			introLabel.setPrefHeight(250);
-			
-			intro.setPrefHeight(247);
 			intro.getChildren().add(introLabel);
 			
 			
+			// Set the top of the GUI to the intro graphic; center to all buttons, labels, and lists to collect user input
 			page.setTop(intro);
 			page.setCenter(root);
 	
@@ -93,19 +101,19 @@ public class Main extends Application {
 			colorOptions.add(new NamedColor(Color.PURPLE, "Purple"));
 			colorOptions.add(new NamedColor(Color.PINK, "Pink"));
 			colorOptions.add(new NamedColor(Color.WHITE, "White"));
-			colorOptions.add(new NamedColor(Color.BLACK, "Black"));
+			colorOptions.add(new NamedColor(Color.GREY, "Grey"));
 			
 			
 
 			// List of shapes selectable in typeShapesLV
 			ObservableList<Integer> shapeOptions = 
-				    FXCollections.observableArrayList();
+				    FXCollections.observableArrayList();			
 			for(int i = 3; i < 11; i++)
 				shapeOptions.add(i);
 			
 
 			
-			// Initialize all objects to be used in FlowPane "controlPane"
+			// Initialize all objects to be used in AnchorPane "root"
 			ComboBox<Integer> numShapesCB = new ComboBox<Integer>(numOptions);
 			ListView<Integer> typeShapesLV = new ListView<Integer>(shapeOptions);
 			ListView<NamedColor> colorShapesLV = new ListView<NamedColor>(colorOptions);
@@ -113,29 +121,33 @@ public class Main extends Application {
 			Label typeShapesLB = new Label(" Choose Shapes:\n(number of sides)");
 			Label colorShapesLB = new Label(" Choose Colors:");
 			Label numShapesLB = new Label("Number of shapes:");
-			Label testLabel = new Label("Test Label");
+			Label testLabel = new Label();
 			Button dealButton = new Button("Deal!");
 			Button quitButton = new Button("Quit");
-
+			Label selectedShapesLabel = new Label();
+			Label selectedColorsLabel = new Label();
+			Button resetButton = new Button("Reset");			
 			
 			
 			// Group typeShapes objects together for easy moving
 			Group typeShapesG = new Group();
 			typeShapesG.getChildren().add(typeShapesLV);
 			typeShapesG.getChildren().add(typeShapesLB);
-			typeShapesLV.setPrefSize(100, 200);
+			typeShapesLV.setPrefSize(100, 150);
 			typeShapesLV.setTranslateY(36);
-			typeShapesG.setTranslateX(50);
-			typeShapesG.setTranslateY(40);
+			typeShapesG.setTranslateX(30);
+			typeShapesG.setTranslateY(20);
+			
 			
 			// Group colorShapes objects together for easy moving
 			Group colorShapesG = new Group();
 			colorShapesG.getChildren().add(colorShapesLV);
 			colorShapesG.getChildren().add(colorShapesLB);
-			colorShapesLV.setPrefSize(100, 200);
+			colorShapesLV.setPrefSize(100, 150);
 			colorShapesLV.setTranslateY(36);
-			colorShapesG.setTranslateX(300);
-			colorShapesG.setTranslateY(40);
+			colorShapesG.setTranslateX(180);
+			colorShapesG.setTranslateY(20);
+			
 			
 			// Group numShapes objects together for easy moving
 			Group numShapesG = new Group();
@@ -143,17 +155,36 @@ public class Main extends Application {
 			numShapesG.getChildren().add(numShapesCB);
 			numShapesCB.setTranslateX(130);
 			numShapesLB.setTranslateY(5);
-			numShapesG.setTranslateX(500);
-			numShapesG.setTranslateY(40);
+			numShapesG.setTranslateX(310);
+			numShapesG.setTranslateY(15);
 
 			
+			// Position and set properties of buttons and labels used on initial screen
 			dealButton.setTranslateX(500);
-			dealButton.setTranslateY(100);
+			dealButton.setTranslateY(180);
 			
 			quitButton.setTranslateX(600);
-			quitButton.setTranslateY(100);
-
+			quitButton.setTranslateY(180);
 			
+			resetButton.setTranslateX(550);
+			resetButton.setTranslateY(180);
+
+			testLabel.setTranslateX(180);
+			testLabel.setTranslateY(220);
+			testLabel.setStyle( "-fx-background-color: #eeeeee; -fx-text-fill: red;  -fx-font-size: 20");
+			
+			selectedShapesLabel.setTranslateX(380);
+			selectedShapesLabel.setTranslateY(40);
+			selectedShapesLabel.setPrefSize(100, 200);
+			selectedShapesLabel.setWrapText(true);
+			
+			selectedColorsLabel.setTranslateX(300);
+			selectedColorsLabel.setTranslateY(40);
+			selectedColorsLabel.setPrefSize(100, 200);
+			selectedColorsLabel.setWrapText(true);
+	
+			colorShapesLV.setStyle("-fx-background-color: blue");
+			typeShapesLV.setStyle("-fx-background-color: blue");
 			
 			
 			
@@ -163,10 +194,17 @@ public class Main extends Application {
 					new ListChangeListener<Integer>() {
 						public void onChanged
 						(ListChangeListener.Change< ? extends Integer> c) {
-							testLabel.setText(typeShapesLV.getSelectionModel().getSelectedItems().toString());
-							selectedShapes = typeShapesLV.getSelectionModel().getSelectedItems();
+							
+							selectedShapes = typeShapesLV.getSelectionModel().getSelectedItems(); // Set selectedShapes list to the selected shapes
+							selectedShapesLabel.setText("SELECTED\nSHAPES:\n");
+							for(Integer i:selectedShapes){
+								selectedShapesLabel.setText(selectedShapesLabel.getText() + i + " sided shape.\n");  // Print selected shapes to a label
+							
+							}
 						}
 					});
+			
+			
 			
 			// Allow the ListView for color of shape to handle multiple selections
 			colorShapesLV.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -174,11 +212,16 @@ public class Main extends Application {
 					new ListChangeListener<NamedColor>() {
 						public void onChanged
 						(ListChangeListener.Change< ? extends NamedColor> c) {
-							testLabel.setText(colorShapesLV.getSelectionModel().getSelectedItems().toString());
-							selectedColors = colorShapesLV.getSelectionModel().getSelectedItems();
-							System.out.println(selectedColors);
+							
+							selectedColors = colorShapesLV.getSelectionModel().getSelectedItems(); // Set selectedColors list to the selected colors
+							selectedColorsLabel.setText("SELECTED\nCOLORS:\n");
+							for(NamedColor i:selectedColors){
+								selectedColorsLabel.setText(selectedColorsLabel.getText() + i + "\n"); // Print selected shapes to a label
+							
+							}
 						}
 					});	
+			
 			
 			
 			// Handle the dropdown menu to select number of cards to deal
@@ -186,63 +229,97 @@ public class Main extends Application {
 		        	public void handle( ActionEvent ae ) {
 		        		
 		        		numberOfCards = numShapesCB.getValue();
-		        		testLabel.setText("" + numberOfCards);
 		        		
-	
 		        	}
 		        });
 			
 			
 			
-	        // Click to button to deal cards
+	        // Click button to deal cards
 	       dealButton.setOnAction( new EventHandler<ActionEvent>() {
 	        	public void handle( ActionEvent ae ) {
+	        		
 	        		Set<NamedColor> possibleColors = new HashSet<NamedColor>();
 	        		Set<Integer> possibleSides = new HashSet<Integer>();
 	        		
+	        		// addAll the user-selected colors and shapes from observable list to sets to be passed to the game
 	        		possibleColors.addAll(selectedColors);
 	        		possibleSides.addAll(selectedShapes);
 	        		
-	        		if(possibleColors != null && possibleSides != null && numberOfCards != -1){
+	        		// Error checking, tests whether or not user has input enough information to start a game
+	        		if(possibleColors.isEmpty()) testLabel.setText("Error: please select at least one color");
+	        		else if(possibleSides.isEmpty()) testLabel.setText("Error: please select at least one shape");
+	        		else if(numberOfCards == -1) testLabel.setText("Error: please select a number of cards");
+	        		else{
+	        				        			
+	        			testLabel.setText("");
+	        			
+	        			// Initialization of game
 	        			Game testGame = new Game(numberOfCards, possibleSides, possibleColors);
 	    				page.getChildren().add(testGame);
-	        			//page.setTop(testGame);
 	    				
-	    				
+	    				// Set the center of the page to root
 	    				page.setCenter(root);
+	    				
+	    				// Set introLabel to not visible in order to show game board
 	    				introLabel.setVisible(false);
 	    				
 	    				
 	    				testGame.setTranslateY(50);
 	    				
-	    				if(numberOfCards == 3)
-	    					testGame.setTranslateX(200);
-	    				else if(numberOfCards == 5)
-	    					testGame.setTranslateX(100);
-	    				
-	    				
+	    				// After user starts the game, they are no longer allowed to re-deal or change selected shapes/colors
 	    				typeShapesG.setDisable(true);
 	    				colorShapesG.setDisable(true);
 	    				dealButton.setDisable(true);
-	    				
-	    				
-	    				}
-	        		else testLabel.setText("Error: please enter correct info");
-	        	
-
+	    					
+	        		}
 	        	}
 	        });
 	       
 
 	       
-	        // Click to button to quit. This will close the window
+	        // Click button to quit. This will close the window
 	        quitButton.setOnAction( new EventHandler<ActionEvent>() {
 	        	public void handle( ActionEvent ae ) {
 	        		primaryStage.close();
 	        	}
 	        });
 		
-
+	        
+	        // Click button to reset. This will bring user back to initial intro screen and reset all necessary values
+	        resetButton.setOnAction( new EventHandler<ActionEvent>() {
+	        	public void handle( ActionEvent ae ) {
+	        		
+	        		// Clear all children of the page (this includes the game, the root pane, and the intro pane)
+    				page.getChildren().clear();
+	        		
+    				// Re-add children to the page, less the game
+    				introLabel.setVisible(true);
+	        		page.setTop(intro);
+	    			page.setCenter(root);
+	    			
+	    			// Clear all listview objects and combobox
+	    			typeShapesLV.getSelectionModel().clearSelection();
+	    			colorShapesLV.getSelectionModel().clearSelection();
+	    			numShapesCB.getSelectionModel().clearSelection();
+	    			
+	    			// Re-inable buttons
+	        		typeShapesG.setDisable(false);
+    				colorShapesG.setDisable(false);
+    				dealButton.setDisable(false);
+    				
+    				// Clear label text
+    				selectedColorsLabel.setText("");
+    				selectedShapesLabel.setText("");
+    				
+    				// Reset variables
+    				numberOfCards = -1;
+    				selectedShapes.clear();
+    				selectedColors.clear();
+    				
+	                
+	        	}
+	        });
 	        
 	        
 	       
@@ -253,11 +330,15 @@ public class Main extends Application {
 			root.getChildren().add(testLabel);
 			root.getChildren().add(dealButton);
 			root.getChildren().add(quitButton);
-
+			root.getChildren().add(selectedColorsLabel);
+			root.getChildren().add(selectedShapesLabel);
+			root.getChildren().add(resetButton);
 			
 
+			
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
